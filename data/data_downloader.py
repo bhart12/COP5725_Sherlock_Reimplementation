5d747f112c12b69e560be63c0c28e72df27d9532
bhart12 <37195929+bhart12@users.noreply.github.com>
2021-03-08 17:32:09 -0500

    Separated training and data download
    Separated training and data download into different files

--- data/data_downloader.py
+++ data/data_downloader.py
@@ -2,10 +2,7 @@ import os
 
 from google_drive_downloader import GoogleDriveDownloader as gd
 
-
-"""Download raw and preprocessed data files.
-The data is downloaded from Google Drive and stored in the 'data/' directory.
-"""
+#Download raw and preprocessed data if they do no already exist
 data_dir = './data.zip'
 print(f"Downloading the raw and preprocessed data into {data_dir}.")
 
@@ -20,3 +17,44 @@ if not os.path.exists(data_dir):
     )
 
 print('Data was downloaded.')
+
+
+
+
+
+
+#Download data files for feature extraction information if they do no already exist
+word_embedding_file = '../sherlock/features/glove.6B.50d.txt'
+paragraph_vector_file = '../sherlock/features/par_vec_trained_400.pkl.docvecs.vectors_docs.npy'
+
+print(
+    f"""Preparing feature extraction by downloading 2 files:
+    \n {word_embedding_file} and \n {paragraph_vector_file}.
+    """
+)
+
+if not os.path.exists(word_embedding_file):
+    print('Downloading GloVe word embedding vectors.')
+    file_name = word_embedding_file
+    gd.download_file_from_google_drive(
+        file_id='1kayd5oNRQm8-NCvA8pIrtezbQ-B1_Vmk',
+        dest_path=file_name,
+        unzip=False,
+        showsize=True
+    )
+
+    print("GloVe word embedding vectors were downloaded.")
+
+if not os.path.exists(paragraph_vector_file):
+    print("Downloading pretrained paragraph vectors.")
+    file_name = paragraph_vector_file
+    gd.download_file_from_google_drive(
+        file_id='1vdyGJ4aB71FCaNqJKYX387eVufcH4SAu',
+        dest_path=file_name,
+        unzip=False,
+        showsize=True
+    )
+
+    print("Trained paragraph vector model was downloaded.")
+
+print("All files for extracting word and paragraph embeddings are present.")
