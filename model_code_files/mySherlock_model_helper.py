750e88a03d781b246ebc6e56140db04895a2920b
bhart12 <37195929+bhart12@users.noreply.github.com>
2021-03-24 17:35:14 -0400

    Added Conrad's model creation
    Added Conrad's code to create model from scratch into the create_sherlock_model function

--- model_code_files/mySherlock_model_helper.py
+++ model_code_files/mySherlock_model_helper.py
@@ -4,48 +4,11 @@ import pandas as pd
 
 from sklearn.preprocessing import LabelEncoder
 
+from tensorflow import keras
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.models import model_from_json
 
 #from sherlock.deploy import model_helpers
-
-
-def _convert_list_of_labels_to_categorical_label_encodings(y_train_values, y_val_values, model_name) -> (list, list):
-    """Encode semantic type string labels as categoricals.
-
-    Parameters
-    ----------
-    y_train_values
-        Train labels.
-    y_val_values
-        Validation labels.
-    model_name
-        Identifier of retrained model.
-
-    Returns
-    -------
-    y_train_cat
-        Categorical encodings of train labels.
-    y_val_cat
-        Categorical encodings of validation labels.
-    """
-
-    #Prepare categorical label encoder
-    encoder = LabelEncoder()
-    encoder.fit(y_train_values)
-
-    #Save labels used in this model to .npy file
-    np.save(f"../sherlock/deploy/classes_{model_name}.npy", encoder.classes_)
-
-    # Convert train labels
-    y_train_int = encoder.transform(y_train_values)
-    y_train_cat = tf.keras.utils.to_categorical(y_train_int)
-
-    # Convert val labels
-    y_val_int = encoder.transform(y_val_values)
-    y_val_cat = tf.keras.utils.to_categorical(y_val_int)
-
-    return y_train_cat, y_val_cat
 
 
 def create_sherlock_model(model_name: str, with_weights: bool):
@@ -56,7 +19,7 @@ def create_sherlock_model(model_name: str, with_weights: bool):
     model_name
         Identifier for retrained model.
     with_weights
-        Whether to populate the model with trained weights.
+        Whether to populate the model with trained weights, or start as untrained.
 
     Returns
     -------
@@ -75,15 +38,55 @@ def create_sherlock_model(model_name: str, with_weights: bool):
     file.close()
 
     if with_weights:
+        #Open previously trained model
         file = open(f"../models/{model_name}_model.json", "r")
         sherlock_model = model_from_json(file.read())
         file.close()
 
         sherlock_model.load_weights(f"../models/{model_name}_weights.h5")
     else:
-        file = open(f"../models/sherlock_model.json", "r")
-        sherlock_model = model_from_json(file.read())
-        file.close()
+        #Create new model from scratch
+        input_shape1 = 960
+        input_shape2 = 201
+        input_shape3 = 400
+        input_shape4 = 27
+
+        # Multi-Input Layers
+        input1 = keras.Input(shape=input_shape1)
+        input2 = keras.Input(shape=input_shape2)
+        input3 = keras.Input(shape=input_shape3)
+        input4 = keras.Input(shape=input_shape4)
+
+        # Hidden Layers
+        batchNorm = keras.layers.BatchNormalization()(input1)
+        batchNorm1 = keras.layers.BatchNormalization()(input2)
+        batchNorm2 = keras.layers.BatchNormalization()(input3)
+        dense = keras.layers.Dense(300, activation='relu')(batchNorm)
+        dense2 = keras.layers.Dense(200, activation='relu')(batchNorm1)
+        dense4 = keras.layers.Dense(400, activation='relu')(batchNorm2)
+        dropout = keras.layers.Dropout(0.3)(dense)
+        dropout1 = keras.layers.Dropout(0.3)(dense2)
+        dropout2 = keras.layers.Dropout(0.3)(dense4)
+        dense1 = keras.layers.Dense(300, activation='relu')(dropout)
+        dense3 = keras.layers.Dense(200, activation='relu')(dropout1)
+        dense5 = keras.layers.Dense(400, activation='relu')(dropout2)
+        batchNorm3 = keras.layers.BatchNormalization()(input4)
+        concatenate = keras.layers.Concatenate()([dense1, dense3, dense5, batchNorm3])
+        batchNorm4 = keras.layers.BatchNormalization()(concatenate)
+        dense6 = keras.layers.Dense(500, activation='relu')(batchNorm4)
+        dropout3 = keras.layers.Dropout(0.3)(dense6)
+        dense7 = keras.layers.Dense(500, activation='relu')(dropout3)
+
+        # Output Layer
+        output = keras.layers.Dense(78, activation='softmax')(dense7)
+
+        sherlock_model = keras.Model(inputs=[input1, input2, input3, input4], outputs=output, name="Sherlock_Reimplementation_Model")
+
+
+        #opt = keras.optimizers.Adam(learning_rate=0.0001)
+        #model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+
+    #print(sherlock_model.summary())
 
     sherlock_model.compile(
         optimizer=tf.keras.optimizers.Adam(lr=lr),
@@ -92,6 +95,47 @@ def create_sherlock_model(model_name: str, with_weights: bool):
     )
 
     return sherlock_model, callbacks
+
+def _convert_list_of_labels_to_categorical_label_encodings(y_train_values, y_val_values, model_name) -> (list, list):
+    """Encode semantic type string labels as categoricals.
+
+    Parameters
+    ----------
+    y_train_values
+        Train labels.
+    y_val_values
+        Validation labels.
+    model_name
+        Identifier of retrained model.
+
+    Returns
+    -------
+    y_train_cat
+        Categorical encodings (binary matrix representation) of train labels.
+    y_val_cat
+        Categorical encodings (binary matrix representation) of validation labels.
+    """
+
+    #Prepare categorical label encoder
+    encoder = LabelEncoder()
+    encoder.fit(y_train_values)
+
+    #Save labels used in this model to .npy file
+    np.save(f"../sherlock/deploy/classes_{model_name}.npy", encoder.classes_)
+
+    #Convert train label strings to normalized encoding
+    #y_train_int = encoder.fit_transform(y_train_values)
+    y_train_int = encoder.transform(y_train_values)
+    #Convert normalized encoding to binary matrix representation of the input
+    y_train_cat = tf.keras.utils.to_categorical(y_train_int)
+
+    #Convert validation label strings to normalized encoding
+    y_val_int = encoder.transform(y_val_values)
+    #Convert normalized encoding to binary matrix representation of the input
+    y_val_cat = tf.keras.utils.to_categorical(y_val_int)
+
+    return y_train_cat, y_val_cat
+
 
 def get_processed_feature_category_dicts() -> dict:
     """Get feature identifiers per feature set, to map features to feature sets.
@@ -160,7 +204,7 @@ def train_sherlock(
             """
         )
 
-    feature_cols = get_processed_feature_category_dicts()
+    feature_column_names = get_processed_feature_category_dicts()
     y_train_categories, y_val_categories = _convert_list_of_labels_to_categorical_label_encodings(y_train_values, y_val_values, model_name)
     sherlock_model, callbacks = create_sherlock_model(model_name, new_model_flag)
 
@@ -170,18 +214,18 @@ def train_sherlock(
 
     sherlock_model.fit(
         [
-            np.asarray(X_train_values[feature_cols['char']].values).astype(np.float32),
-            np.asarray(X_train_values[feature_cols['word']].values).astype(np.float32),
-            np.asarray(X_train_values[feature_cols['par']].values).astype(np.float32),
-            np.asarray(X_train_values[feature_cols['rest']].values).astype(np.float32)
+            np.asarray(X_train_values[feature_column_names['char']].values).astype(np.float32),
+            np.asarray(X_train_values[feature_column_names['word']].values).astype(np.float32),
+            np.asarray(X_train_values[feature_column_names['par']].values).astype(np.float32),
+            np.asarray(X_train_values[feature_column_names['rest']].values).astype(np.float32)
         ],
         y_train_categories,
         validation_data=(
             [
-                np.asarray(X_val_values[feature_cols['char']].values).astype(np.float32),
-                np.asarray(X_val_values[feature_cols['word']].values).astype(np.float32),
-                np.asarray(X_val_values[feature_cols['par']].values).astype(np.float32),
-                np.asarray(X_val_values[feature_cols['rest']].values).astype(np.float32)
+                np.asarray(X_val_values[feature_column_names['char']].values).astype(np.float32),
+                np.asarray(X_val_values[feature_column_names['word']].values).astype(np.float32),
+                np.asarray(X_val_values[feature_column_names['par']].values).astype(np.float32),
+                np.asarray(X_val_values[feature_column_names['rest']].values).astype(np.float32)
             ],
             y_val_categories
         ),
